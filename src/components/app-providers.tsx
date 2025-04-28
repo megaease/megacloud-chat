9fff4c1cbbb0fc8475e9d60b84bcc236179980ae
whhjdi <wanghandi95@gamil.com>
2025-04-28 17:23:58 +0800

    feat: add theme provider and mode toggle components; refactor layout to include theme support

--- src/components/app-providers.tsx
+++ src/components/app-providers.tsx
@@ -8,23 +8,33 @@ import {
 } from "@/components/ui/sidebar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./mode-toggle";
 
 const queryClient = new QueryClient();
 export default function Page({ children }: React.PropsWithChildren) {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<SidebarProvider>
-				<AppSidebar />
-				<SidebarInset className="flex h-dvh flex-col">
-					<header className="relative flex shrink-0 items-center gap-2 border-b px-4">
-						<div className="absolute top-4 left-4 z-50">
-							<SidebarTrigger />
-						</div>
-					</header>
-					<div className="flex-1 overflow-auto">{children}</div>
-				</SidebarInset>
-			</SidebarProvider>
-			<Toaster richColors position="top-right" />
-		</QueryClientProvider>
+		<ThemeProvider
+			attribute="class"
+			defaultTheme="system"
+			enableSystem
+			disableTransitionOnChange
+		>
+			<QueryClientProvider client={queryClient}>
+				<SidebarProvider>
+					<AppSidebar />
+					<SidebarInset className="flex h-dvh flex-col">
+						<header className="relative flex shrink-0 items-center gap-2 border-b px-4 justify-between py-2">
+							<div className="">
+								<SidebarTrigger />
+							</div>
+							<ModeToggle />
+						</header>
+						<div className="flex-1 overflow-auto">{children}</div>
+					</SidebarInset>
+				</SidebarProvider>
+				<Toaster richColors position="top-right" />
+			</QueryClientProvider>
+		</ThemeProvider>
 	);
 }
