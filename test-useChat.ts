61131729658f790c8200211c90b4c40883929156
whhjdi <wanghandi95@gamil.com>
2025-08-12 16:31:04 +0800

    chore: Delete multiple test files to clean up the code base

--- test-useChat.ts
+++
@@ -1,8 +0,0 @@
-import { useChat } from "@ai-sdk/react";
-
-// Test to see what useChat actually returns
-const chatHooks = useChat();
-console.log("chatHooks keys:", Object.keys(chatHooks));
-
-// This will show us the actual available properties
-type ChatHooksType = typeof chatHooks;
